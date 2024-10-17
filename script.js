9daf72d3494566bf2943fba1886e9e8150865516
JIOs <jorgemilio1011@gmail.com>
2024-10-16 21:25:28 -0500

    pagina web reponsive estructura semifinal

---
+++ script.js
@@ -0,0 +1,8 @@
+// Interactividad del menú desplegable
+const mobileMenu = document.getElementById('mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+
+mobileMenu.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active');
+    navLinks.classList.toggle('active');
+});
